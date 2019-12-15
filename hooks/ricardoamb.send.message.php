cdb3c8961c00f0db96e38da167cf5a8b87eaa073
Ricardo Amb <ricardoamb.dev@gmail.com>
2019-12-15 06:32:28 -0300

    changing the domain

--- hooks/ricardoamb.send.message.php
+++ hooks/ricardoamb.send.message.php
@@ -15,14 +15,14 @@ if ( $nome !== null && $email !== null && $mensagem !== null && $hash === 'zz$Qe
 {
 
     $from = $nome . '<' . $email . '>';
-    $to = "eu@ricardoamb.com";
-    $subject = 'Mensagem de ' . $nome . ' - ricardoamb.com';
+    $to = "eu@ricardoamb.github.io";
+    $subject = 'Mensagem de ' . $nome . ' - ricardoamb.github.io';
 
     setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
     date_default_timezone_set('America/Sao_Paulo');
 
     $message = '<div class="email-container" style="padding-top:30px;border-top:5px solid #336699;max-width:600px;">';
-    $message .= '<div style="display:block;text-align:center;"><img src="https://ricardoamb.com/assets/images/brand-dark.svg" style="width:240px;height:auto;"></div>';
+    $message .= '<div style="display:block;text-align:center;"><img src="https://ricardoamb.github.io/assets/images/brand-dark.svg" style="width:240px;height:auto;"></div>';
     $message .= '<div style="display:block;width:100%;text-align:center;margin-bottom:30px;font-family:sans-serif;margin-top:20px;"><h1 style="color:#336699;">Mensagem do Site</h1></div>';
     $message .= '<div style="font-family:sans-serif;display:block;color:#6f6f6f;">Mensagem enviada em:' . strftime('%A, %d de %B de %Y', strtotime('today')) . '</div>';
     $message .= '<div style="font-family:sans-serif;display:block;">Mensagem de : <strong>' . $nome . '</strong></div>';
